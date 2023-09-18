f1be9a09a0086403a7a60041d1ec91c14c64ee04
windblood <shuaiminrising@gmail.com>
2023-09-18 13:24:36 +0800

    change readme rst to markdown

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 long_description = ''
 
 if 'upload' in sys.argv:
-    with open('README.rst') as f:
+    with open('README.md') as f:
         long_description = f.read()
 
 install_reqs = [
