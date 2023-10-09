f7371cb5546b6427677965cbe7d8f429d9aae5f5
windblood <shuaiminrising@gmail.com>
2023-10-09 09:42:57 +0800

    update preprocess.py

--- alphalens/preprocess.py
+++ alphalens/preprocess.py
@@ -3,6 +3,26 @@
 """
 data preprocess utils for factor analysis
 
+factors : pd.DataFrame - MultiIndex
+        A MultiIndex DataFrame indexed by timestamp (level 0) and asset
+        (level 1), containing the values for factors.
+        ::
+            ---------------------------------------
+                       |       | f1  | f2  | f3
+            ---------------------------------------
+                date   | asset |     |     |
+            ---------------------------------------
+                       | AAPL  | 0.09|-0.01|-0.079
+                       ----------------------------
+                       | BA    | 0.02| 0.06| 0.020
+                       ----------------------------
+            2014-01-01 | CMG   | 0.03| 0.09| 0.036
+                       ----------------------------
+                       | DAL   |-0.02|-0.06|-0.029
+                       ----------------------------
+                       | LULU  |-0.03| 0.05|-0.009
+                       ----------------------------
+
 @author:
 @date: 2023/9/20
 """
@@ -12,12 +32,52 @@ from sympy import Matrix, GramSchmidt
 
 
 # na/nan values process
+def process_na(factors, method='fill', **kwargs):
+    res = factors
+    if method == 'drop':
+        res = factors.dropna()
+    elif method == 'fill':
+        value = kwargs.get('value', 0)
+        if isinstance(value, (int, float)):
+            res = factors.fillna(value=value)
+        elif value in ['ffill', 'bfill']:
+            res = factors.groupby(level=1).fillna(method=value)
+        elif value == 'mean':
+            res = factors.groupby(level=0).apply(lambda df: df.fillna(value=df.mean()))
+        elif value == 'median':
+            res = factors.groupby(level=0).apply(lambda df: df.fillna(value=df.median()))
+
+    return res
 
 
-# outlier/inf process
+# outliers/inf process
+def process_outliers(factors, method='std', **kwargs):
+    res = factors
+    if method == 'std':
+        n = kwargs.get('n', 3)
+    elif method == 'mad':
+        n = kwargs.get('n', 3)
+    elif method == 'percentile':
+        percentiles = kwargs.get('percentile', (0.05, 0.95))
+        if isinstance(percentiles, (int, float)):
+            if percentiles >= 1:
+                percentiles = percentiles / 100
+            percentiles = sorted((1 - percentiles, percentiles))
+
+    return res
 
 
 # normalization
+def normalize(factors, method=None, **kwargs):
+    res = factors
+    if method == 'minmax':
+        pass
+    elif method == 'zscore':
+        pass
+    elif method == 'rank':
+        pass
+
+    return res
 
 
 # neutralization
@@ -57,24 +117,6 @@ def get_orthogonal_factors(factors, method='Schimidt'):
     Parameters
     ----------
     factors : pd.DataFrame - MultiIndex
-        A MultiIndex DataFrame indexed by timestamp (level 0) and asset
-        (level 1), containing the values for factors.
-        ::
-            ---------------------------------------
-                       |       | f1  | f2  | f3
-            ---------------------------------------
-                date   | asset |     |     |
-            ---------------------------------------
-                       | AAPL  | 0.09|-0.01|-0.079
-                       ----------------------------
-                       | BA    | 0.02| 0.06| 0.020
-                       ----------------------------
-            2014-01-01 | CMG   | 0.03| 0.09| 0.036
-                       ----------------------------
-                       | DAL   |-0.02|-0.06|-0.029
-                       ----------------------------
-                       | LULU  |-0.03| 0.05|-0.009
-                       ----------------------------
 
     method : str
         Orthogonal method: 'Schimidt', 'Symmetry', 'Canonical'
@@ -82,31 +124,13 @@ def get_orthogonal_factors(factors, method='Schimidt'):
     Returns
     -------
     orthogonal_factors: pd.DataFrame - MultiIndex
-        A MultiIndex DataFrame indexed by date (level 0) and asset (level 1),
-        containing the values for orthogonal factors for each period, each asset.
-        ::
-           ---------------------------------------
-                       |       | f1  | f2  | f3
-            ---------------------------------------
-                date   | asset |     |     |
-            ---------------------------------------
-                       | AAPL  | 0.09|-0.01|-0.079
-                       ----------------------------
-                       | BA    | 0.02| 0.06| 0.020
-                       ----------------------------
-            2014-01-01 | CMG   | 0.03| 0.09| 0.036
-                       ----------------------------
-                       | DAL   |-0.02|-0.06|-0.029
-                       ----------------------------
-                       | LULU  |-0.03| 0.05|-0.009
-                       ----------------------------
     """
     dates = factors.index.get_level_values(level='date').unique()
     factor_names = factors.columns
     new_factors = []
     for dt in dates:
         tmp_factor = factors.loc[dt]
-        tmp_new = _single_orthogonal_factors(tmp_factor.values)
+        tmp_new = _single_orthogonal_factors(tmp_factor.values, method=method)
         tmp_new = pd.DataFrame(tmp_new, index=tmp_factor.index, columns=factor_names)
         tmp_new['date'] = dt
         tmp_new.set_index(['date', tmp_new.index], inplace=True)
@@ -146,12 +170,11 @@ def orthogonal_factor(factor, base_factors, method='Schimidt'):
 
 
 if __name__ == '__main__':
-    arr = np.random.randn(100, 5)
     indices = [['2020-01-01', '2020-02-01', '2020-03-01', '2020-04-01', '2020-05-01'], range(20)]
     indices = pd.MultiIndex.from_product(iterables=indices, names=['date', 'asset'])
     columns = ['f1', 'f2', 'f3', 'f4', 'f5']
-    arr = pd.DataFrame(arr, index=indices, columns=columns)
+    df = pd.DataFrame(np.random.randn(100, 5), index=indices, columns=columns)
     # new_df = get_orthogonal_factors(arr, 'Schimidt')
     f6 = pd.DataFrame(np.random.randn(100, 1), index=indices, columns=['f6'])
-    new_f6 = orthogonal_factor(f6, arr, 'Canonical')
+    new_f6 = orthogonal_factor(f6, df, 'Canonical')
     print('ok')
